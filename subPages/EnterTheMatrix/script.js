cb9970fa47d5067d91093ab35ef9bed0699a0c82
DmitriyRumyantsev <Guitarmaster1703@mail.ru>
2020-10-31 19:51:31 +0700

    added js

--- subPages/EnterTheMatrix/script.js
+++ subPages/EnterTheMatrix/script.js
@@ -1,3 +1,32 @@
-var circle = new Path2D();
-    circle.moveTo(125, 35);
-    circle.arc(100, 35, 25, 0, 2 * Math.PI);
+let w = window.innerWidth;
+let h = window.innerHeight;
+let ctx = document.getElementById("canvas").getContext("2d");
+
+ctx.beginPath();
+ctx.arc(50, 50, 25, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(50, 150, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(50, 250, 25/2, 0, 2 * Math.PI);
+ctx.stroke();
+
+console.log( w);
+console.log( h);
+
+//рисуем дом
+
+ctx.beginPath();
+ctx.moveTo(200,150); //начальная точка
+ctx.lineTo(300,150); 
+ctx.lineTo(250,75); //конечная вершина
+ctx.fill();
+
+ctx.rect(200, 150, 100, 100);
+ctx.stroke();
+
+ctx.rect(240, 200, 25, 50);
+ctx.stroke();
