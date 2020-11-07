669e7e39949a01c57b81d6d0652e0c137a0d64df
DmitriyRumyanstev <Guitarmaster1703@mail.ru>
2020-11-07 20:04:22 +0700

    ended js

--- subPages/EnterTheMatrix/script.js
+++ subPages/EnterTheMatrix/script.js
@@ -23,10 +23,33 @@ ctx.beginPath();
 ctx.moveTo(200,150); //начальная точка
 ctx.lineTo(300,150); 
 ctx.lineTo(250,75); //конечная вершина
+ctx.fillStyle = "brown";
 ctx.fill();
 
 ctx.rect(200, 150, 100, 100);
 ctx.stroke();
 
 ctx.rect(240, 200, 25, 50);
+ctx.fillStyle = "grey";
+ctx.fillRect(240, 200, 25, 50);
+
+
+let i;
+
 ctx.stroke();
+
+let counter = 0, x=0,y=180;
+//100 iterations
+let increase = 90/180*Math.PI / 9;
+for(i=0; i<=360; i+=10){
+    
+     ctx.moveTo(x,y);
+    x = i;
+    y =  180 - Math.sin(counter) * 120;
+counter += increase;
+     
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    //alert( " x : " + x + " y : " + y + " increase : " + counter ) ;
+
+}
